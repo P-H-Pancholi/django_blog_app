be941b1cb395bbaafffc20aed4f951c263f3b32f
P-H-Pancholi <pancholiphalin@outlook.com>
2022-03-12 16:20:18 +0530

    Added context

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
+posts = [
+	{
+		'author' : 'Phill',
+		'title' : 'First post',
+		'content' : 'My first post',
+		'date_posted' : '13 Nov 2020'
+	},
+	{
+		'author' : 'Phalin',
+		'title' : 'second post',
+		'content' : 'My second post',
+		'date_posted' : '14 Nov 2020'
+	}
+]
+
 # Create your views here.
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {'posts' : posts}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
 	return render(request,'blog/about.html')
