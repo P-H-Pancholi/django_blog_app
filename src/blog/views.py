292ccfa18c8bc0d2d98c9587bce205fed3c02808
P-H-Pancholi <pancholiphalin@outlook.com>
2022-04-10 18:11:18 +0530

    Added post to views and registered post model

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-	{
-		'author' : 'Phill',
-		'title' : 'First post',
-		'content' : 'My first post',
-		'date_posted' : '13 Nov 2020'
-	},
-	{
-		'author' : 'Phalin',
-		'title' : 'second post',
-		'content' : 'My second post',
-		'date_posted' : '14 Nov 2020'
-	}
-]
+
 
 # Create your views here.
 def home(request):
-	context = {'posts' : posts}
+	context = {'posts' : Post.objects.all()}
 	return render(request, 'blog/home.html', context)
 
 def about(request):
